569a05f491570a033979b1ce8d4faf0f29359ac5
Jack Maney <jackmaney@gmail.com>
2013-07-19 17:25:31 -0500

    Finished and (informally) tested printInserttionSortDiagram(). For fun, that's why!

--- src/com/jackmaney/IntroductionToAlgorithms/mainInsertionSort.java
+++ src/com/jackmaney/IntroductionToAlgorithms/mainInsertionSort.java
@@ -16,9 +16,25 @@ public class mainInsertionSort {
 		
 		//System.out.println(a);
 		
-		Vector<Integer> b = new Vector<Integer>(Arrays.asList(2,4,5,6,1,3));
+		/*Vector<Integer> b = new Vector<Integer>(Arrays.asList(2,4,5,6,1,3));
 		
-		System.out.println(InsertionSort.getMoves(b));
+		System.out.println(InsertionSort.getMoves(a));
+		
+		InsertionSort.applyNextMove(a);
+		
+		System.out.println(a);
+		
+		System.out.println(InsertionSort.getMoves(a));
+		
+		InsertionSort.applyNextMove(a);
+		
+		System.out.println(a);
+		
+		Vector<Integer> c = new Vector<Integer>(Arrays.asList(1,2,3,4,5,6));
+		
+		System.out.println(InsertionSort.getMoves(c));*/
+		
+		InsertionSort.printInsertionSortDiagram(a);
 		
 	}
 
--- src/com/jackmaney/IntroductionToAlgorithms/sort/InsertionSort.java
+++ src/com/jackmaney/IntroductionToAlgorithms/sort/InsertionSort.java
@@ -27,11 +27,6 @@ public class InsertionSort{
 		
 	}
 	
-	//The median of the n numbers 0,1,2,...,n-1 is (n-1)/2.
-	private float medianIndex(int n){
-		 
-		return (float)(n-1) / 2;
-	}
 	
 	//Each element returned is a 1 for "moves right", 
 	//-n for "moves left n times" (n > 0, of course), and 0 for staying put
@@ -67,11 +62,145 @@ public class InsertionSort{
 		return result;
 	}
 	
+	private <T> String join(AbstractList<T> list,String delimiter){
+		
+		String result = null;
+		
+		if(!list.isEmpty()){
+			StringBuilder sb = new StringBuilder();
+			sb.append(list.get(0).toString());
+			
+			for(int i = 1; i < list.size(); i++){
+				sb.append(delimiter).append(list.get(i).toString());
+			}
+			
+			result = sb.toString();
+		}
+		
+		return result;
+	}
+	
+	private <T> String join(AbstractList<T> list){
+		return join(list," ");
+	}
+	
+	public static <T extends Comparable<T>>  
+		boolean applyNextMove(AbstractList<T> list,ArrayList<Integer> moves){
+		
+		/*
+		 * We traverse from right to left, looking for the first (and only)
+		 * negative entry of moves, call it -m at index i. Then, we temporarily store
+		 * the element at index i, shuffle the elements from index i-m to i-1 
+		 * to the right one, and then put the stored element at index i-m.
+		 */
+		for(int i = moves.size() - 1; i >= 0; i--){
+			
+			if(moves.get(i) < 0){
+				T element = list.get(i);
+				for(int j = i - 1; j >= i + moves.get(i); j--){
+					list.set(j+1,list.get(j));
+				}
+				
+				list.set(i + moves.get(i), element);
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static <T extends Comparable<T>>  
+		boolean applyNextMove(AbstractList<T> list){
+		
+		ArrayList<Integer> moves = getMoves(list);
+		
+		return applyNextMove(list,moves);
+	}
+	
+	
+	
+	
+	
 	public static <T extends Comparable<T>> 
 		void printInsertionSortDiagram(AbstractList<T> list){
 		
-		
-		
+		while(true){
+			
+			ArrayList<Integer> moves = getMoves(list);
+			
+			int indexToMove = -1;
+			int amountToMoveLeft = 1;
+			
+			
+			
+			for(int i = 0; i < moves.size(); i++){
+				
+				if(moves.get(i) < 0){
+					indexToMove = i;
+					amountToMoveLeft = moves.get(i);
+					break;
+				}
+			}
+			
+			if(indexToMove < 0){
+				break;
+			}
+			
+			int firstIndexForPipe = -1;
+			int lastIndexForPipe = -1;
+			int indexCounter = 0;
+			
+			System.out.print(list.get(0));
+			if(moves.get(0) > 0){
+				firstIndexForPipe = 0;
+				indexCounter = list.get(0).toString().length();
+			}
+			
+			for(int i = 1; i < moves.size(); i++){
+				
+				if(moves.get(i-1) > 0){
+					System.out.print(" -> ");
+					indexCounter += 4;
+				}
+				else{
+					System.out.print(" ");
+					indexCounter++;
+				}
+				
+				
+				if(moves.get(i) > 0){
+					if(firstIndexForPipe < 0){
+						firstIndexForPipe = indexCounter + 1;
+					}
+				}
+				else if(moves.get(i) < 0){
+					lastIndexForPipe = indexCounter + 1;
+				}
+				
+				System.out.print(list.get(i));
+				indexCounter += list.get(i).toString().length();
+			}
+			
+			System.out.println();
+			
+			for(int i = 0; i < firstIndexForPipe; i++){
+				System.out.print(" ");
+			}
+			
+			System.out.print("|");
+			
+			for(int i = firstIndexForPipe + 1; i < lastIndexForPipe - 1; i++){
+				System.out.print("_");
+			}
+			
+			System.out.println("|");
+			System.out.println();
+			
+			if(!InsertionSort.applyNextMove(list)){
+				break;
+			}
+			
+		}
 		
 	}
 	
