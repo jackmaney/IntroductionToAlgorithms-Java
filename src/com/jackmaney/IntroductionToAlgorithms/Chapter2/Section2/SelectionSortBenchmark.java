c5312ff227c9aaafcd5e680acfce73d2553b15cb
Jack Maney <jackmaney@gmail.com>
2013-08-15 18:39:32 -0500

    Forgot to add benchmarks...also, for some reason, Eclipse wouldn't add SelectionSortBenchmark.java to the repo automatically. Dunno why.

---
+++ src/com/jackmaney/IntroductionToAlgorithms/Chapter2/Section2/SelectionSortBenchmark.java
@@ -0,0 +1,49 @@
+package com.jackmaney.IntroductionToAlgorithms.Chapter2.Section2;
+
+import java.util.ArrayList;
+
+import org.perfidix.annotation.AfterEachRun;
+import org.perfidix.annotation.BeforeEachRun;
+import org.perfidix.annotation.Bench;
+import org.perfidix.annotation.BenchClass;
+
+@BenchClass(runs = 10)
+public final class SelectionSortBenchmark {
+
+	private static ArrayList<Double> randomArrayList(int n){
+		if(n <= 0){
+			throw new IllegalArgumentException();
+		}
+		
+		ArrayList<Double> result = new ArrayList<>();
+		
+		for(int i = 0; i < n; i++){
+			result.add(Math.random());
+		}
+		
+		return result;
+	}
+	
+	private ArrayList<Double> a;
+	
+	@BeforeEachRun
+	public void setUp(){
+		a = randomArrayList(2000);
+	}
+	
+	@AfterEachRun
+	public void tearDown(){
+		a = null;
+	}
+	
+	@Bench
+	public void nonRecursiveSort(){
+		NonRecursiveSelectionSort.sort(a);
+	}
+	
+	@Bench
+	public void recursiveSort(){
+		SelectionSort.sort(a);
+	}
+	
+}
