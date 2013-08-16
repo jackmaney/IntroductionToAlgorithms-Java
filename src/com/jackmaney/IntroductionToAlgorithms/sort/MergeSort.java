45878973cb95f217b046458c71b85c1b6836d4ea
Jack Maney <jackmaney@gmail.com>
2013-08-16 12:15:55 -0500

    Added Merge Sort and a unit test

---
+++ src/com/jackmaney/IntroductionToAlgorithms/sort/MergeSort.java
@@ -0,0 +1,72 @@
+package com.jackmaney.IntroductionToAlgorithms.sort;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+
+public class MergeSort {
+	
+	public static <T extends Comparable<T>> void sort(AbstractList<T> list,int p, int r){
+		if(p < r){
+			int q = (int)Math.floor( ( (double)p + (double)r )/2);
+			sort(list,p,q);
+			sort(list,q+1,r);
+			merge(list,p,q,r);
+		}
+	}
+	
+	public static <T extends Comparable<T>> void sort(AbstractList<T> list){
+		sort(list,0,list.size() - 1);
+	}
+	
+	public static <T extends Comparable<T>> void merge(AbstractList<T> list,int p, int q, int r){
+		
+		if(p > q || q >= r || r >= list.size() || p < 0){
+			throw new IllegalArgumentException();
+		}
+		
+		
+		/*
+		 * We want to replace merge elements between indices p and q (inclusive)
+		 * to elements with indices q+1 to r (inclusive).
+		 * 
+		 */
+		
+		ArrayList<T> left = new ArrayList<>();
+		ArrayList<T> right = new ArrayList<>();
+		
+		for(int i = 0; i <= q - p; i++){
+			left.add(list.get(p + i));
+		}
+		
+		for(int j = 1; j <= r - q; j++){
+			right.add(list.get(q+j));
+		}
+
+		
+		for(int k = p; k <= r; k++){
+			
+			T element = null;
+			
+			if(left.isEmpty() && right.isEmpty()){
+				throw new ArrayIndexOutOfBoundsException();
+			}
+			else if(left.isEmpty()){
+				element = right.remove(0);
+			}
+			else if(right.isEmpty()){
+				element = left.remove(0);
+			}
+			else if(left.get(0).compareTo(right.get(0)) <= 0){
+				element = left.remove(0);
+			}
+			else
+			{
+				element = right.remove(0);
+			}
+			
+			list.set(k,element);
+		}
+		
+	}
+	
+}
---
+++ test/com/jackmaney/IntroductionToAlgorithms/sort/MergeSortTest.java
@@ -0,0 +1,31 @@
+package com.jackmaney.IntroductionToAlgorithms.sort;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class MergeSortTest {
+
+	
+	private ArrayList<Integer> sorted = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7));
+
+	@Test
+	public void mergeTest() {
+		ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(1,3,6,2,4,5,7));
+		
+		MergeSort.merge(a, 0, 2, 6);
+		
+		assertTrue(a.equals(sorted));
+	}
+	
+	@Test
+	public void mergeSortTest(){
+		ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(1,3,6,2,4,5,7));
+		MergeSort.sort(a);
+		assertTrue(a.equals(sorted));
+	}
+
+}
